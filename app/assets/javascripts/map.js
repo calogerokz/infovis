e821f0a4da8a7550b1d850e5dc5cc79181ac6d5a
Andrei Sacuiu <andrei.sacuiu@uni-konstanz.de>
2017-06-07 17:56:09 +0200

    added example for how to get

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,3 +1,9 @@
 $( document ).ready(function() {
     $("#monthSlider").slider({});
+    $("#monthSlider").slider().on('slideStop', function(ev){
+        $("#startDate").html($("#monthSlider").data('slider').getValue()[0]);
+        $("#endDate").html($("#monthSlider").data('slider').getValue()[1]);
+    });
+
 });
+
