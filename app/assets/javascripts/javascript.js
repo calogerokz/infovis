4c0c97b95199e586181ebd683e2cb046a6ce8a5e
YING ZHANG <sydying@wl-02105.WLAN.LOCAL>
2017-06-08 12:42:52 +0200

    ghbjhbj

--- app/assets/javascripts/javascript.js
+++ app/assets/javascripts/javascript.js
@@ -2,14 +2,9 @@
 var request = function() {
     var input = $("#inputQ").val();
     $.ajax({
-        url: "/query/"+input,
+        url: "/query/"+"select id from random311",
     }).done(function( data ) {
-        for (i=0; i<data[0].length; i++) {
-            for (j=0; j<data[0][i][0].length; j++) {
-                $("#display").append(data[0][i][0][j]+" ");
-            }
-            $("#display").append("<br />");
-        }
+        console.log(data)
   });
 };
 
